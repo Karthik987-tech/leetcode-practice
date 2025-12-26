724687d53ba8b5ef614860f059b787cc5b4c605d
Karthik987-tech <yenumulakarthik6028@gmail.com>
2025-12-26 06:27:44 +0000

    Sync LeetCode submission Runtime - 517 ms (99.63%), Memory - 0.0B (100.00%)

--- 0183-customers-who-never-order/solution.sql
+++ 0183-customers-who-never-order/solution.sql
@@ -1,3 +1,8 @@
 /* Write your PL/SQL query statement below */
-select name as customers from customers
- where id not in(select customerid from orders);  
+select c.name as customers from
+customers c
+left join
+orders o
+on c.id=o.customerid 
+where o.id is NULL ;
+
